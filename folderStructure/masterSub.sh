0e5b5ab6051a21d12f17b24618ddc85979675e43
RichGraham <Richard.Graham@nottingham.ac.uk>
2017-01-19 15:11:24 +0000

    Changed the hpc sub script for a more uptodate project code

--- folderStructure/masterSub.sh
+++ folderStructure/masterSub.sh
@@ -11,8 +11,8 @@
 # This send your output to the file P1.out rather than the standard filename
 #PBS -N  <NAME>
 # Sets the job name
-#PBS -P  HPCA-02061-PGR 
-# Sets the job project ( HPCA-00544-PGR = Tom;  HPCA-02061-PGR  = George)
+#PBS -P  HPCA-00544-PGR
+# Sets the job project ( HPCA-00544-PGR = Tom;  HPCA-02932-EFR  = Leeds FIC)
 # OPTIONS FOR PBS PRO ==============================================================
 # Here we just use Unix command to run our program
 
